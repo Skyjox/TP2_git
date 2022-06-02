8308a1f226f869d0333a18edb84598d6eb485ffa
Laurent Richir <laurent.richir@icloud.com>
2022-06-02 14:03:39 +0200

    test darkmode
    test darkmode sur page touquet

--- tpp8/intro.js
+++ tpp8/intro.js
@@ -921,63 +921,61 @@ $(document).ready(function(){
          $(".photo_body_sub_T-B_overlay10-1").show();
       });
       //10eme overlay fin
-      $("#sun").click(function(){
-         if ($("body").hasClass("dark")){
-             $("body").removeClass("dark");
-             $("#llogo").attr('src','../logoDM-removebg-preview.png');
-             $("#sun").removeClass('moon');
-             $("#sun").attr('src','../js/soleil-removebg-preview.png');
-             $('.menu').removeClass("menu-dark");
-             $(".lvideo-background").removeClass("lvdm");
-             $(".lieux").removeClass("lieuxdm");
-             $("#logo2").attr('src','../img/logo-removebg-preview.png');
-             $("#txtb1").removeClass('txtdm');
-             $("#txtb2").removeClass('txtdm');
-             $("#txtb3").removeClass('txtdm');
-             $("#txtb4").removeClass('txtdm');
-             $("#txtb5").removeClass('txtdm');
-             $("#txtb6").removeClass('txtdm');
-             $("#txtb7").removeClass('txtdm');
-             $("#txtb8").removeClass('txtdm');
-             $("#txtb9").removeClass('txtdm');
-             $("#txtb10").removeClass('txtdm');
+      $("#sun").click(function () {
+         if ($("body").hasClass("dark")) {
+           $("body").removeClass("dark");
+           $(".bglinear").removeClass("dark");
+           $("#llogo").attr("src", "../img/logo-removebg-preview.png");
+           $(".l_lille").removeClass("golden");
+           $("#sun").removeClass("moon");
+           $("#sun").attr("src", "../js/soleil-removebg-preview.png");
+           $(".menu").removeClass("menu-dark");
+           $(".lvideo-background").removeClass("lvdm");
+           $(".lieux").removeClass("lieuxdm");
+           $("#logo2").attr("src", "../img/logo-removebg-preview.png");
+           $("#txtb1").removeClass("txtdm");
+           $("#txtb2").removeClass("txtdm");
+           $("#txtb3").removeClass("txtdm");
+           $("#txtb4").removeClass("txtdm");
+           $("#txtb5").removeClass("txtdm");
+           $("#txtb6").removeClass("txtdm");
+           $("#txtb7").removeClass("txtdm");
+           $("#txtb8").removeClass("txtdm");
+           $("#txtb9").removeClass("txtdm");
+           $("#txtb10").removeClass("txtdm");
+         } else {
+           $("body").addClass("dark");
+           $(".bglinear").addClass("dark");
+           $("#llogo").attr("src", "../img/logoDM-removebg-preview-copie.png");
+           $(".l_lille").addClass("golden");
+           $("#sun").addClass("moon");
+           $("#sun").attr("src", "../js/moon-removebg-preview.png");
+           $(".menu").addClass("menu-dark");
+           $(".lieux").addClass("lieuxdm");
+           $("#logo2").attr("src", "../img/logoDM-removebg-preview-copie.png");
+           $(".lvideo-background").addClass("lvdm");
+           $("#txtb1").addClass("txtdm");
+           $("#txtb2").addClass("txtdm");
+           $("#txtb3").addClass("txtdm");
+           $("#txtb4").addClass("txtdm");
+           $("#txtb5").addClass("txtdm");
+           $("#txtb6").addClass("txtdm");
+           $("#txtb7").addClass("txtdm");
+           $("#txtb8").addClass("txtdm");
+           $("#txtb9").addClass("txtdm");
+           $("#txtb10").addClass("txtdm");
          }
-         else{
-             $("body").addClass("dark");
-             $(".bglinear").addClass("dark");
-             $("#llogo").attr('src', '../js/logoDM-removebg-preview.png');
-             $(".l_lille").addClass('golden');
-             $("#sun").addClass('moon');
-             $("#sun").attr('src','../js/moon-removebg-preview.png');
-             $(".menu").addClass("menu-dark");
-             $(".lieux").addClass("lieuxdm");
-             $("#logo2").attr('src', '../js/logoDM-removebg-preview.png');
-             $(".lvideo-background").addClass("lvdm");
-             $("#txtb1").addClass("txtdm");
-             $("#txtb2").addClass("txtdm");
-             $("#txtb3").addClass("txtdm");
-             $("#txtb4").addClass("txtdm");
-             $("#txtb5").addClass("txtdm");
-             $("#txtb6").addClass("txtdm");
-             $("#txtb7").addClass("txtdm");
-             $("#txtb8").addClass("txtdm");
-             $("#txtb9").addClass("txtdm");
-             $("#txtb10").addClass("txtdm");
- 
- 
- 
-         }
-     });
-     /*---------------------------------------------------*/
-     /*---------------------------------------------------*/
-     /*--------------------carrousel----------------------*/
-     $('figure').slick({
-       autoplay: true,
-       infinite: true,
-       fade: true,
-       cssEase:'ease-in-out',
-       speed: 1000,
-       nextArrow:false,
-       prevArrow:false,
-     });
+       });
+       /*---------------------------------------------------*/
+       /*---------------------------------------------------*/
+       /*--------------------carrousel----------------------*/
+       $("figure").slick({
+         autoplay: true,
+         infinite: true,
+         fade: true,
+         cssEase: "ease-in-out",
+         speed: 1000,
+         nextArrow: false,
+         prevArrow: false,
+       });
 });
